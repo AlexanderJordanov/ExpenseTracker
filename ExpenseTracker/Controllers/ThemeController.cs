334849708532fecbb4a2d20c871db4c4c51368bc
AlexanderJordanov <tafdokidoki9708@gmail.com>
2025-11-24 13:35:59 +0200

    Added Bootswatch themes support + theme selector UI improvements

---
+++ ExpenseTracker/Controllers/ThemeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ThemeController : Controller
+    {
+        public IActionResult Set(string theme, string? returnUrl = "/")
+        {
+            // позволяваме само нашите 3 теми
+            var allowedThemes = new[] { "lux", "cosmo", "darkly" };
+
+            if (!allowedThemes.Contains(theme))
+            {
+                theme = "lux";
+            }
+
+            Response.Cookies.Append("theme", theme, new CookieOptions
+            {
+                Expires = DateTimeOffset.UtcNow.AddYears(1),
+                IsEssential = true
+            });
+
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                returnUrl = "/";
+            }
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
